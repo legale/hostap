6135022143945de62396173fdb66c2da134ddf39
Isaev Ruslan <legale.legale@gmail.com>
2025-01-27 05:28:09 +0300

    add mgmt response wifimon msg
    Signed-off-by: Isaev Ruslan <legale.legale@gmail.com>

--- src/utils/state_machine.h
+++ src/utils/state_machine.h
@@ -46,7 +46,7 @@ static void sm_ ## machine ## _ ## state ## _Enter(STATE_MACHINE_DATA *sm, \
 #define SM_ENTRY(machine, state) \
 if (!global || sm->machine ## _state != machine ## _ ## state) { \
 	sm->changed = true; \
-	wpa_printf(MSG_DEBUG, STATE_MACHINE_DEBUG_PREFIX ": " #machine \
+	WPA_MSG_WIFI_INF(4, STATE_MACHINE_DEBUG_PREFIX ": " #machine \
 		   " entering state " #state); \
 } \
 sm->machine ## _state = machine ## _ ## state;
@@ -65,7 +65,7 @@ sm->machine ## _state = machine ## _ ## state;
 #define SM_ENTRY_M(machine, _state, data) \
 if (!global || sm->data ## _ ## state != machine ## _ ## _state) { \
 	sm->changed = true; \
-	wpa_printf(MSG_DEBUG, STATE_MACHINE_DEBUG_PREFIX ": " \
+	WPA_MSG_WIFI_INF(4, STATE_MACHINE_DEBUG_PREFIX ": " \
 		   #machine " entering state " #_state); \
 } \
 sm->data ## _ ## state = machine ## _ ## _state;
@@ -83,7 +83,7 @@ sm->data ## _ ## state = machine ## _ ## _state;
 #define SM_ENTRY_MA(machine, _state, data) \
 if (!global || sm->data ## _ ## state != machine ## _ ## _state) { \
 	sm->changed = true; \
-	wpa_printf(MSG_DEBUG, STATE_MACHINE_DEBUG_PREFIX ": " MACSTR " " \
+	WPA_MSG_WIFI_INF(4, STATE_MACHINE_DEBUG_PREFIX ": sa=" MACSTR " " \
 		   #machine " entering state " #_state, \
 		   MAC2STR(STATE_MACHINE_ADDR)); \
 } \
@@ -99,10 +99,10 @@ sm->data ## _ ## state = machine ## _ ## _state;
  * move the state machine to a new state.
  */
 #define SM_ENTER(machine, state) \
-    do { \
-        WPA_MSG_WIFI_INF(4, "%s", #state); \
-        sm_ ## machine ## _ ## state ## _Enter(sm, 0); \
-    } while (0)
+do { \
+    WPA_MSG_WIFI_INF(4, "%s", #machine "_" #state); \
+    sm_ ## machine ## _ ## state ## _Enter(sm, 0); \
+} while (0)
 
 /**
  * SM_ENTER_GLOBAL - Enter a new state machine state based on global rule
